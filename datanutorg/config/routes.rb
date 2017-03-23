65f6f1b71ab60a1d82b76bc15ed1c433d14d8f69
Wood Programmer <emirozbir@hotmail.com>
2017-03-23 23:03:09 +0200

    Localtower added

--- datanutorg/config/routes.rb
+++ datanutorg/config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   post 'datanut/index'
   get 'datanut/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  if Rails.env.development?
+    mount Localtower::Engine, at: "localtower"
+  end
 end
